bc45b547ccbe1e744d915decf5c609601d020f14
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-04-10 15:23:35 +0200

    Untested LaTeX template.

--- src/scripts/plot.py
+++ src/scripts/plot.py
@@ -1,8 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import cached_property
 import parallel_hnsw as h
 from pathlib import Path
 from matplotlib import pyplot as plt
+
+BACK_SLASH = "\\"
+N = "\n"
+TAB = "\t"
 
 class AppError(Exception):
 	pass
@@ -26,6 +30,9 @@ class Point:
 	x: float
 	y: float
 
+	def getTupleStr(self):
+		return f"({self.x}, {self.y})"
+
 @dataclass
 class Line:
 	name: str
@@ -39,6 +46,66 @@ class Line:
 
 	def plot(self):
 		plt.plot(self.getXArray(), self.getYArray(), label=self.name)
+
+@dataclass
+class GroupPlot:
+	lines: list[Line]
+	metric: str
+
+	def getLatex(self):
+		res = f"{BACK_SLASH}nextgroupplot[title={self.metric}]{N}"
+
+		for line in self.lines:
+			res += f"{BACK_SLASH}addplot coordinates {{{N}% {line.name}{N}"
+
+			for p in line.points:
+				res += f"{TAB}{p.getTupleStr()}{N}"
+
+			res += f"}};{N}"
+
+		return res
+
+	def getLegend(self):
+		return ",".join([l.name for l in self.lines])
+
+@dataclass
+class GroupPlotPair:
+	build: GroupPlot = None
+	recall: GroupPlot = None
+
+@dataclass
+class Plot:
+	bestDirection: str
+	caption: str
+	groups: list[GroupPlot]
+	plotLabel: str
+	xLabel: str
+	yLabel: str
+	shortCaption: str = None
+	yModeLog: bool = False
+
+	def getLatex(self, template: str):
+		return template.replace("@GROUP_SIZE@", "2 by 1"
+		).replace("@YMODE@", f"{N}ymode = log," if self.yModeLog else ""
+		).replace("@XLABEL@", self.xLabel
+		).replace("@YLABEL@", self.yLabel
+		).replace("@GROUP_PLOTS@", "\n\n".join([g.getLatex() for g in self.groups])
+		).replace("@LEGEND@", self.groups[0].getLegend()
+		).replace("@SHORT_CAPTION@", f"[{self.shortCaption}]" if self.shortCaption is not None else ""
+		).replace("@LONG_CAPTION@", self.caption
+		).replace("@BEST_DIRECTION@", self.bestDirection
+		).replace("@LABEL@", self.plotLabel)
+
+	def writeLatex(self, plotsDir: Path, template: str):
+		with (plotsDir / f"{self.plotLabel}.tex").open("w", encoding="utf-8") as f:
+			f.write(self.getLatex(template))
+
+@dataclass
+class SpaceGroupPlots:
+	bestSIMD: GroupPlotPair = field(default_factory=GroupPlotPair)
+	noSIMD: GroupPlotPair = field(default_factory=GroupPlotPair)
+	SIMDadv: GroupPlotPair = field(default_factory=GroupPlotPair)
+	SIMDseq: GroupPlotPair = field(default_factory=GroupPlotPair)
 
 class Stats:
 	def __init__(self, b: h.Benchmark):
@@ -107,20 +174,67 @@ def plot(
 	plt.ylabel(yLabel)
 	fig.savefig(plotsDir / f"{name}.svg")
 	plt.show()
+	return GroupPlot(lines, metric)
 
 def plotBuild(dim: int, metric: str, plotsDir: Path, name: str, *statsCol: list[Stats]):
-	plot(
+	return plot(
 		[getBuildLine(s) for s in statsCol], dim=dim, metric=metric,
 		xLabel="Počet prvků při stavbě", yLabel="Čas stavby (s)",
 		plotsDir=plotsDir, name=name
 	)
 
 def plotRecall(dim: int, metric: str, trainCount: int, plotsDir: Path, name: str, *stats: Stats):
-	plot(
+	return plot(
 		[s.getRecallLine() for s in stats], dim=dim, metric=metric,
 		xLabel="Přesnost", yLabel="Počet dotazů za sekundu (1/s)",
 		plotsDir=plotsDir, name=name, trainCount=trainCount
 	)
+
+def writeLatexPlots(
+	angularPlots: SpaceGroupPlots, euclideanPlots: SpaceGroupPlots,
+	plotsDir: Path, template: str
+):
+	buildPlot = Plot(
+		bestDirection="Lepší výsledky směrem k dolnímu okraji grafu.",
+		caption="Závislost času stavby na počtu prvků při stavbě.",
+		groups=[euclideanPlots.noSIMD.build, angularPlots.noSIMD.build],
+		plotLabel="NoSIMDBuild", xLabel="Počet prvků při stavbě",
+		yLabel="Čas stavby (s)"
+	)
+	buildPlot.writeLatex(plotsDir, template)
+
+	buildPlot.groups=[euclideanPlots.SIMDseq.build, angularPlots.SIMDseq.build]
+	buildPlot.plotLabel = "SIMDseqBuild"
+	buildPlot.writeLatex(plotsDir, template)
+
+	buildPlot.groups=[euclideanPlots.SIMDadv.build, angularPlots.SIMDadv.build]
+	buildPlot.plotLabel = "SIMDadvBuild"
+	buildPlot.writeLatex(plotsDir, template)
+
+	buildPlot.groups=[euclideanPlots.bestSIMD.build, angularPlots.bestSIMD.build]
+	buildPlot.plotLabel = "bestSIMDBuild"
+	buildPlot.writeLatex(plotsDir, template)
+
+	recallPlot = Plot(
+		bestDirection="Lepší výsledky směrem k pravému hornímu rohu grafu.",
+		caption="Závislost počtu zpracovaných dotazů za sekundu na přesnosti.",
+		groups=[euclideanPlots.noSIMD.recall, angularPlots.noSIMD.recall],
+		plotLabel="NoSIMDRecall", xLabel="Přesnost",
+		yLabel=r"Počet dotazů za sekundu $(\frac{1}{s})$"
+	)
+	recallPlot.writeLatex(plotsDir, template)
+
+	recallPlot.groups=[euclideanPlots.SIMDseq.recall, angularPlots.SIMDseq.recall]
+	recallPlot.plotLabel = "SIMDseqRecall"
+	recallPlot.writeLatex(plotsDir, template)
+
+	recallPlot.groups=[euclideanPlots.SIMDadv.recall, angularPlots.SIMDadv.recall]
+	recallPlot.plotLabel = "SIMDadvRecall"
+	recallPlot.writeLatex(plotsDir, template)
+
+	recallPlot.groups=[euclideanPlots.bestSIMD.recall, angularPlots.bestSIMD.recall]
+	recallPlot.plotLabel = "bestSIMDRecall"
+	recallPlot.writeLatex(plotsDir, template)
 
 @dataclass
 class BenchmarkList:
@@ -150,13 +264,13 @@ class BenchmarkList:
 		return list(self.seqStats.values()) if trainCount is None else self.seqStats[trainCount]
 
 	def plotBuild(self, plotsDir: Path, name: str):
-		plotBuild(
+		return plotBuild(
 			self.dim, getCzechMetric(self.space), plotsDir, name,
 			self.getSeqStats(), *self.getParStats(),
 		)
 
 	def plotRecall(self, trainCount: int, plotsDir: Path, name: str):
-		plotRecall(
+		return plotRecall(
 			self.dim, getCzechMetric(self.space), trainCount, plotsDir, name,
 			self.seqStats[trainCount], *[self.parStats[w][trainCount] for w in self.workerCounts]
 		)
@@ -188,15 +302,21 @@ def getBenchmarkList(cfg: Config, simdType: h.SIMDType, space: h.Space):
 	return res
 
 def run(cfg: Config):
-	plotsDir = Path(__file__).parents[1] / "plots"
+	src = Path(__file__).parents[1]
+	plotsDir = src / "plots"
 	plotsDir.mkdir(exist_ok=True)
-	runForSpace(cfg, h.Space.EUCLIDEAN, plotsDir)
-	runForSpace(cfg, h.Space.ANGULAR, plotsDir)
+	euclideanPlots = runForSpace(cfg, h.Space.EUCLIDEAN, plotsDir)
+	angularPlots = runForSpace(cfg, h.Space.ANGULAR, plotsDir)
+	writeLatexPlots(
+		angularPlots, euclideanPlots, plotsDir,
+		(src / "templates" / "groupPlots.txt").read_text(encoding="utf-8")
+	)
 
 def runForSpace(cfg: Config, space: h.Space, plotsDir: Path):
+	res = SpaceGroupPlots()
 	noSIMDBenchmarks = getBenchmarkList(cfg, h.SIMDType.NONE, space)
-	noSIMDBenchmarks.plotBuild(plotsDir, f"no_simd_build_{getEnglishMetric(space)}")
-	noSIMDBenchmarks.plotRecall(
+	res.noSIMD.build = noSIMDBenchmarks.plotBuild(plotsDir, f"no_simd_build_{getEnglishMetric(space)}")
+	res.noSIMD.recall = noSIMDBenchmarks.plotRecall(
 		cfg.maxTrainCount, plotsDir,
 		f"no_simd_recall_{getEnglishMetric(space)}"
 	)
@@ -204,36 +324,41 @@ def runForSpace(cfg: Config, space: h.Space, plotsDir: Path):
 	availableSIMD = getAvailableSIMD()
 
 	if not availableSIMD:
-		return
+		return res
 
 	SIMDBenchmarks = {SIMD: getBenchmarkList(cfg, SIMD, space) for SIMD in availableSIMD}
 	bestSIMDBenchmarks = SIMDBenchmarks[h.getBestSIMDType()]
 
-	plotBuild(
+	res.SIMDseq.build = plotBuild(
 		cfg.dim, getCzechMetric(space), plotsDir, f"simd_sequential_build_{getEnglishMetric(space)}",
 		noSIMDBenchmarks.getSeqStats(),
 		*[b.getSeqStats() for b in SIMDBenchmarks.values()]
 	)
-	plotRecall(
+	res.SIMDseq.recall = plotRecall(
 		cfg.dim, getCzechMetric(space), cfg.maxTrainCount, plotsDir,
 		f"simd_sequential_recall_{getEnglishMetric(space)}",
 		noSIMDBenchmarks.getSeqStats(cfg.maxTrainCount),
 		*[b.getSeqStats(cfg.maxTrainCount) for b in SIMDBenchmarks.values()]
 	)
-	plotBuild(
+	res.SIMDadv.build = plotBuild(
 		cfg.dim, getCzechMetric(space), plotsDir,
 		f"bestSIMD_sequential_vs_noSIMD_parallel_build_{getEnglishMetric(space)}",
 		bestSIMDBenchmarks.getSeqStats(),
 		*noSIMDBenchmarks.getParStats()
 	)
-	plotRecall(
+	res.SIMDadv.recall = plotRecall(
 		cfg.dim, getCzechMetric(space), cfg.maxTrainCount, plotsDir,
 		f"bestSIMD_sequential_vs_noSIMD_parallel_recall_{getEnglishMetric(space)}",
 		bestSIMDBenchmarks.getSeqStats(cfg.maxTrainCount),
 		*noSIMDBenchmarks.getParStats(cfg.maxTrainCount)
 	)
-	bestSIMDBenchmarks.plotBuild(plotsDir, f"best_simd_build_{getEnglishMetric(space)}")
-	bestSIMDBenchmarks.plotRecall(cfg.maxTrainCount, plotsDir, f"best_simd_recall_{getEnglishMetric(space)}")
+	res.bestSIMD.build = bestSIMDBenchmarks.plotBuild(
+		plotsDir, f"best_simd_build_{getEnglishMetric(space)}"
+	)
+	res.bestSIMD.recall = bestSIMDBenchmarks.plotRecall(
+		cfg.maxTrainCount, plotsDir, f"best_simd_recall_{getEnglishMetric(space)}"
+	)
+	return res
 
 def main():
 	run(Config(
